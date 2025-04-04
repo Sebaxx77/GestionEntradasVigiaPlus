f8700cba62f9818ff08bc167bd0d0096b2ebd23d
Sebaxx77 <juanpiamba23@gmail.com>
2025-04-04 15:34:06 -0500

    Se modifica el modelo user y el factory para User ya que con la instalación de jetstream se sobrescribieron algunas modificaciones.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Operacion;
 use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
 
@@ -24,12 +26,13 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('12345'),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
             'profile_photo_path' => null,
             'current_team_id' => null,
+            'operacion_id' => Operacion::inRandomOrder()->first()->id, // Asigna un operacion_id aleatorio
         ];
     }
 
