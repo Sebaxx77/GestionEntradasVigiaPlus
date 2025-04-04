75758c1aea37310f56dad73e8146ccf0b709ef32
Sebaxx77 <juanpiamba23@gmail.com>
2025-04-04 15:28:50 -0500

    Se instala el Starter Kit Jetstream.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasRoles, HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -32,6 +37,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -41,13 +48,14 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
     /**
-     * Relación: Muchos usuarios pueden pertenecer a una operación.
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
      */
-    public function operacion() 
-    {
-        return $this->belongsTo(Operacion::class);
-    }
+    protected $appends = [
+        'profile_photo_url',
+    ];
 }
